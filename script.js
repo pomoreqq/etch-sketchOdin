73cd0a89928e4348bf58470f434bdb6b62231f7d
pomoreqq <tomasz.pomorski99@gmail.com>
2024-08-26 13:45:38 +0200

    completed dynamic grid function

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-const sketchContainer = document.querySelector('#sketchContainer')
 
 
 
+document.addEventListener('DOMContentLoaded', () => {
+    createGrid(16)
+
+})
+
+function createGrid(num) {
+    const gridContainersNumber = num * num
+    const container = document.querySelector('.container')
+
+   const gridContainerSize = 100 / num;
+    
+    for (let i = 0; i < gridContainersNumber; i++) {
+        const baseDiv = document.createElement('div')
+        baseDiv.classList.add('gridDiv')
+        baseDiv.style.flex = `0 0 ${gridContainerSize}%`
+        baseDiv.style.height = `${gridContainerSize}%`
+        container.appendChild(baseDiv)
+    }
+
+   
+}
 
 
 
